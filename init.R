7ab3812b1b75d24d9395da0b4952cd2355c4fa9c
Astrowests <Astrowests@users.noreply.github.com>
2020-12-16 02:55:26 +0700

    fixed

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("shiny", "leaflet","glue","DBI","RPostgres","inlmisc")
+my_packages = c("shiny", "leaflet","glue","DBI","RPostgres","inlmisc","rgdal","rgeos")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {

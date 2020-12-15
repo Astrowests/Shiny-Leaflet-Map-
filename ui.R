aa5731c11a323670dfde68bb847d025c84665e7a
Astrowests <Astrowests@users.noreply.github.com>
2020-12-16 02:32:29 +0700

    fixed

--- ui.R
+++ ui.R
@@ -10,8 +10,33 @@
 library(shiny)
 library(leaflet)
 
+library(DBI)
+library(RPostgres)
+library(glue)
+database <- dbConnect(drv = Postgres(),user='asehbulwsrcsmr',password='42c67e62a08d87402c1218a7451b1a0216f6018c5a1e2d87eb0ec884b1d24add',host='ec2-52-22-238-188.compute-1.amazonaws.com',port=5432,dbname='d25mhv2io94ahk')
 
 
+dataframe <- dbGetQuery(database,"select namagedunggereja,lokasigedunggereja,namakating,nowa,namajeniskegiatan,jadwalgerejawi,latitudegedunggereja,longitudegedunggereja from gerejawi inner join kating on kating.idkating = gerejawi.idkating inner join gedunggereja
+on gedunggereja.idgedunggereja=gerejawi.idgedunggereja inner join jeniskegiatan on jeniskegiatan.idjeniskegiatan = gerejawi.idjeniskegiatan
+")
+
+for (a in unique(dataframe$namagedunggereja)) {
+    temp <- dataframe[dataframe$namagedunggereja==a,]
+    popup <- glue("<b>{temp$namagedunggereja}</b><br>
+                <a>wa.me/{temp$nowa[1]}</a>
+                {temp$namajeniskegiatan[1]} : {temp$jadwalgerejawi[1]} ,<br>
+                {temp$namajeniskegiatan[2]} : {temp$jadwalgerejawi[2]} ,<br>
+               {temp$namajeniskegiatan[3]} : {temp$jadwalgerejawi[3]} ,<br>
+               {temp$namajeniskegiatan[4]} : {temp$jadwalgerejawi[4]} ,<br>
+               {temp$namajeniskegiatan[5]} : {temp$jadwalgerejawi[5]}")
+    m <- leaflet()%>%
+        addTiles() %>% 
+        setView(lng = unique(temp$longitudegedunggereja),lat = unique(temp$latitudegedunggereja), zoom = 12) %>% 
+        addMarkers(popup = unique(popup),clusterOptions = markerClusterOptions(),group ="markers",label =  unique(temp$namagedunggereja),icon =makeIcon("~/UKK UTY/Map Project/mapsku/mapsku/salib.png",iconWidth = 40,iconHeight = 40),lng = unique(temp$longitudegedunggereja),lat = unique(temp$latitudegedunggereja)) %>% 
+        addPopups(popup = "Click mee twice",lng = unique(temp$longitudegedunggereja),lat = unique(temp$latitudegedunggereja)) %>% 
+        inlmisc::AddSearchButton(group = "markers",textPlaceholder = "Nama Gereja") 
+    
+}
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
     titlePanel("Data Kegiatan Gereja beserta penyebaran UKK di"),
